800a1561fc1f2c80304fbf660f1e438864898ad5
Jan Slavik <janslavikgreenfox@gmail.com>
2025-01-03 12:25:57 +0100

    Removed the unused code in the Baseline header file

--- StaticLibrary/include/Baseline.h
+++ StaticLibrary/include/Baseline.h
@@ -5,16 +5,6 @@
 namespace ConsoleAppRansacIINamespace {
 namespace Core {
 
-typedef double	columnValuesType;
-typedef int		headerRowIndexType;
-typedef int		valuesRowIndexType;
-typedef int		columnIndexType;
-
-enum CTestResult {
-	Success = 0,
-	Failure = 1
-};
-
 constexpr double epsilon{ std::numeric_limits<double>::epsilon() };
 
 } // namespace Core
