502e921953023fd548b656588cdab3335a750f9e
Jan Slavik <janslavikgreenfox@gmail.com>
2025-01-08 17:06:40 +0100

    The Table export modified that it is a concretion of an abstraction (i.e. the interface ITableExport)

--- StaticLibrary/include/TableExport.h
+++ StaticLibrary/include/TableExport.h
@@ -9,6 +9,134 @@ namespace ConsoleAppRansacIINamespace {
 namespace IO {
 
 /**
+* @class ITableExport interface
+*/
+class ITableExport {
+public:
+	/**
+	* @brief Exports a table somehow.
+	*/
+	virtual void exportTable() const = 0;
+
+	/**
+	* @brief Gets the string that represents the exported table.
+	* @return The string that represents the exported table.
+	*/
+	virtual std::string getExportedString() const = 0;
+
+	/**
+	* @brief Destructor
+	*
+	* @note If a class has at least one virtual method, it should also have a virtual destructor.
+	*/
+	virtual ~ITableExport() = default;
+};
+
+/**
+* @class TableExportToString
+*/
+class TableExportToCsvBase : public ITableExport {
+  public:
+	/**
+	* @brief Constructor for the TableExportToString class.
+	* @param ptrTable The pointer to a table to export.
+	* @param delimiter The delimiter character.
+	*/
+	  TableExportToCsvBase(std::shared_ptr<Table> ptrTable, const char delimiter = ',')
+		  : _pTable{ std::move(ptrTable) }, _delimiter{}, _csvString{} {};
+
+	/**
+	* @brief Exports a table to a CSV string.
+	*/
+	virtual void exportTable() const override;
+
+	/**
+	* @brief Gets the CSV string.
+	*/
+	std::string getExportedString() const override { return _csvString; }
+
+  protected:
+	/**
+	* @brief Generates a CSV string.
+	* 
+	*/
+	void generateCsvString();
+
+  private:
+   /**
+   * @brief The table to export.
+   */
+   std::shared_ptr<Table> _pTable;
+   
+   /**
+   * @brief The delimiter character.
+   */
+   char _delimiter;
+
+   /**
+   * @brief The CSV string.
+   */
+   std::string _csvString;
+};
+
+/**
+* @class TableExportToCsvFile
+*/
+class TableExportToCsvFile : public TableExportToCsvBase {
+
+  public:
+	/**
+	* @brief Constructor for the TableExportToCsvFile class.
+	* @param table The table to export.
+	* @param fileName The name of the CSV file.
+	* @param delimiter The delimiter character.
+	*/
+	TableExportToCsvFile(
+		std::shared_ptr<Table> ptrTable, const std::string fileName, const char delimiter)
+		: TableExportToCsvBase{ std::move(ptrTable), delimiter  }, _fileName{ fileName }
+    {
+		TableExportToCsvBase::generateCsvString();
+	};
+
+	/**
+	* @brief Exports a table to a CSV file.
+	*/
+	virtual void exportTable() const override;
+
+	/**
+	* @brief Gets the CSV string.
+	*/
+	std::string getExportedString() const override { 
+		return TableExportToCsvBase::getExportedString(); 
+	};
+
+private:
+	/**
+	* @brief The table to export.
+	*/
+	std::shared_ptr<Table> _pTable;
+
+	/**
+	* @brief The name of the CSV file.
+	*/
+	std::string _fileName;
+
+	/**
+	* @brief The delimiter character.
+	*/
+	char _delimiter;
+
+	/**
+	*
+	*/
+	std::string _csvString;
+};
+
+/**
+* @class TableExportToFile
+*/
+
+/**
 * @class TableExport
 * @brief A class that exports a table to a CSV file or string.
 */
--- StaticLibrary/src/TableExport.cpp
+++ StaticLibrary/src/TableExport.cpp
@@ -7,6 +7,32 @@ using Table = ConsoleAppRansacIINamespace::Core::Table;
 namespace ConsoleAppRansacIINamespace {
 namespace IO {
 
+void TableExportToCsvBase::exportTable() const {
+}
+
+void TableExportToCsvBase::generateCsvString() {
+	// Table name
+	_csvString = std::string(" ");
+	_csvString = std::string(_pTable->getName() + std::string("\n"));
+
+	// Columns headers
+	for (size_t columnIndex = 0; columnIndex < _pTable->getNoOfColumns(); columnIndex++) {
+		_csvString += _pTable->getColumn(columnIndex).getHeader() + _delimiter;
+	}
+	_csvString += "\n";
+
+	// Table data
+	size_t currentNumberOfRows = _pTable->getColumn(0).getNoRows();
+	size_t currentNumberOfColumns = _pTable->getNoOfColumns();
+	for (size_t rowIndex = 0; rowIndex < currentNumberOfRows; rowIndex++) {
+		for (size_t columnIndex = 0; columnIndex < currentNumberOfColumns; columnIndex++) {
+			_csvString += std::to_string(_pTable->getCellValue(rowIndex, columnIndex)) + _delimiter;
+		}
+		_csvString += "\n";
+	}
+}
+
+
 std::string TableExport::ExportToCsvString(const Table& table, const char delimiter) const {
 	
 	// Table name
@@ -28,6 +54,16 @@ std::string TableExport::ExportToCsvString(const Table& table, const char delimi
 		csvOutput += "\n";
 	}
 	return csvOutput;
+}
+
+
+void TableExportToCsvFile::exportTable() const {
+	//generateCsvString();
+	//_csvString = getExportedString();
+	std::ofstream exportFile;
+	exportFile.open(_fileName);
+	exportFile << getExportedString(); //_csvString;
+	exportFile.close();
 }
 
 void TableExport::ExportToCsvFile(const Table& table, std::string fileName = "exportTable.csv", const char delimiter = ',') const {
