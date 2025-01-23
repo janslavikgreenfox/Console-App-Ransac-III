ad15f4ae40a920e1d0d0aa9ce715660c6a4e7768
Jan Slavik <janslavikgreenfox@gmail.com>
2025-01-23 23:12:54 +0100

    Added the simple, trivial test of the RANSAC fit algorithm implementation

---
+++ UnitTest/TestOfRANSACFitStrategy.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2025 Jan Slavik, Blackstone Labs
+// 
+// This file is part of the Console-App-Ransac-III project.
+// 
+// Licensed under the MIT License. You may obtain a copy of the License at:
+// 
+//     https://opensource.org/licenses/MIT
+// 
+// This software is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "Column.h"
+#include "Table.h"
+#include "RANSACFitStrategy.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using Column = ConsoleAppRansacIINamespace::Core::Column;
+using LinearModel = ConsoleAppRansacIINamespace::Core::LinearModel;
+using RANSACFitStrategy = ConsoleAppRansacIINamespace::Fitting::RANSACFitStrategy;
+
+namespace UnitTest
+{
+	TEST_CLASS(RANSACFitTest)
+	{
+	public:
+
+		TEST_METHOD(TrivialCase)
+		{
+			// Arrange
+			std::vector<double> x{ 0.0, 1.0 };
+			Column xColumn{ x, "Column X" };
+			std::vector<double> y{ 2.0, 0.0 };
+			Column yColumn{ y, "Column Y" };
+			constexpr double expectedSlope{ -2.0 };
+			constexpr double expectedYIntercept{ 2.0 };
+
+			LinearModel linearModel;
+			RANSACFitStrategy ransacFitStrategy;
+
+			// Act
+			linearModel = ransacFitStrategy.fitLinearModel(xColumn, yColumn);
+
+			// Assert
+			Assert::AreEqual(expectedSlope, linearModel.getSlope());
+			Assert::AreEqual(expectedYIntercept, linearModel.getValueAt0());
+		}
+	};
+}
