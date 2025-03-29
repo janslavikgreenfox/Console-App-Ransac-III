c50330b8ebb69a745d1229c6c75b487441699cf3
Jan Slavik <janslavikgreenfox@gmail.com>
2025-03-29 17:03:38 +0100

    Added the IColumn interface for the column object

---
+++ StaticLibrary/IColumn.h
@@ -0,0 +1,77 @@
+// Copyright (C) 2025 Jan Slavik, Blackstone Labs
+// 
+// This file is part of the Console-App-Ransac-III project.
+// 
+// Licensed under the MIT License. You may obtain a copy of the License at:
+// 
+//     https://opensource.org/licenses/MIT
+// 
+// This software is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace ConsoleAppRansacIINamespace {
+namespace Core {
+
+	class IColumn
+	{
+	public:
+
+		/**
+		* @brief Destructor
+		*/
+		virtual ~IColumn() {};
+
+		/**
+		* @brief Add a row to the column values
+		* @param value The double value to be added as a new row
+		*/
+		virtual void addRow(const double value) = 0;
+
+		/**
+		* @brief Get number of rows in the column
+		* @return Number of the column rows
+		*/
+		virtual size_t getNoOfRows() const = 0;
+
+		/**
+		* @brief Get the column header
+		* @return The header of the column
+		*/
+		virtual std::string getHeader() const = 0;
+
+		/**
+		* @brief Get all rows from the Column
+		* @return A vector of all the values in the column
+		*/
+		virtual std::vector<double> getAllRows() const = 0;
+
+		/**
+		* @brief Get the value at a specified row index
+		* @param specifiedRowIndex The index of the row to get the value from
+		* @return The value at the specified row index
+		*/
+		virtual double getOneRow(const size_t& specifiedRowIndex) const = 0;
+
+		/**
+		* @brief Get the values at specified row indexes
+		* @param specifiedRowIndexes A vector of picked indexes of the rows to get the values from
+		* @return A vector of the values at the specified row indexed
+		*/
+		virtual IColumn getSpecifiedRows(const std::vector<size_t>& specifiedRowIndexes) const = 0;
+
+		/**
+		* @brief Get the average of the values in the column
+		* @return The average of the values in the column
+		*/
+		virtual double getAverage() const = 0;
+
+	};
+
+} // namespace Core
+} // namespace ConsoleAppRansacIINamespace
